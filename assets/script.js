e25db19b9bf66405817aa0aac6d37139db09ac10
lukas <eiglerhardingl@gmail.com>
2020-03-07 15:58:08 -0500

    updated fetch

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,29 @@
 var Site = {}
+Site.items = []
 document.addEventListener('DOMContentLoaded', init)
 
 
 function init(){
 
 	Site.container = document.getElementById("container")
-	
-	Site.items = [
-		{
-			title: "title",
-			subtitle: "subtitle",
-			url: "/feral",
-			imagepath: "/files/",
-			style: {
-				left : "2vw",
-				top: "50vh"
-			}
-		}
-	]
 
-	Site.loadingItems(Site.items, Site.container)
 
+	return fetch("/files/images.json")
+	.then((r) => r.json())
+	.then((responseJson) => {
+
+		Site.items = responseJson;
+
+		// generate random start positions + end positions
+		
+		// add start + end positions to items
+
+		Site.loadingItems(Site.items, Site.container)
+
+	})
+	.catch((error) => {
+		console.log(error)
+	})
 }
 
 Site.loadingItems = (items, container) => {
@@ -32,7 +35,7 @@ Site.loadingItems = (items, container) => {
 Site.itemComponent = (item, index) => {
 	return `<li class="item" id="item-${index}" style="left: ${item.style.left}; top: ${item.style.top};">
 		<a href="${item.url}">
-			<img src="${item.imagepath}">
+			<img loading="lazy" src="${item.imagepath}">
 			<aside>
 				<h1>${item.title}</h1>
 				<h2>${item.subtitle}</h2>
