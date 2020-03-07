fbc1c2e9c2aab9135eba50d21e5335f6782f3272
Rahul S. Shinde <rshinde@mica.edu>
2020-03-07 18:28:11 -0500

    new js, and css for hover state styling

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,13 @@ function init(){
 	Site.container = document.getElementById("container")
 
 
-	return fetch("/files/images.json")
+	return fetch("../files/images.json")
 	.then((r) => r.json())
 	.then((responseJson) => {
 
 		Site.items = responseJson;
 
+		Site.generateStyles();
 		// generate random start positions + end positions
 		
 		// add start + end positions to items
@@ -32,13 +33,39 @@ Site.loadingItems = (items, container) => {
 	})
 }
 
+Site.generateStyles = () => {
+	var d = new Date();
+	var minute = d.getUTCMinutes()
+
+
+}
+
 Site.itemComponent = (item, index) => {
-	return `<li class="item" id="item-${index}" style="left: ${item.style.left}; top: ${item.style.top};">
+	// list item gets one class for animation keyframes
+	// item fades in and out, added to animatino keyframes
+	// list item gets one class defines start and end using translation
+	// ten points of starting on each side
+	// 2 possible animations per side and direction, upper lower and middle
+	// middle gets either
+	// item gets inline styles for speed
+	// item gets inline-styles for easing
+
+	// item_image gets inline styles for bobbing speed
+	// item_image also has an inline bobbing delay
+	// item_image gets inline-styles for bobbing easing
+	// Lazy loading of images
+
+	return `<li class="item" id="item-${index}">
 		<a href="${item.url}">
-			<img loading="lazy" src="${item.imagepath}">
-			<aside>
-				<h1>${item.title}</h1>
-				<h2>${item.subtitle}</h2>
+			<img loading="lazy" class="item_image" src="${item.imagepath}">
+			<aside class="text_container">
+				<h1 class="item_title item_text">${item.title}</h1>
+				<div class="categories ${item.category.toLowerCase()}">
+					<span class="item_category invasion">Invasion</span>
+					<span class="item_category capital">Capital</span>
+					<span class="item_category acceleration">Acceleration</span>
+					<span class="item_category empire">Empire</span>
+				</div>
 			</aside>
 		</a>
 	</li>`
