55c4c2e12ca49af72fdaca646310dc90cae3a515
lukas <eiglerhardingl@gmail.com>
2020-03-07 19:50:05 -0500

    udpating wiht animation integration

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # project-feral_atlas
+
+
+
+- [x] define order of array
+- [x] select 15
+- [ ] keep track of count
+- [ ] add delays (random)
+- [ ] add duration (random)
+- [ ] callback on complete
+	- remove
+	- add next item
+	- if(last item)
+		- redefine order of array
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var Site = {}
 Site.items = []
+Site.itemTracker = 15;
 document.addEventListener('DOMContentLoaded', init)
 
 
@@ -7,18 +8,15 @@ function init(){
 
 	Site.container = document.getElementById("container")
 
-
 	return fetch("../files/images.json")
 	.then((r) => r.json())
 	.then((responseJson) => {
 
-		Site.items = responseJson;
-
-		Site.generateStyles();
-		// generate random start positions + end positions
+		if(!Array.isArray(responseJson)){ return; }
+		console.log("what")
+		Site.items = Site.arrayOrder(responseJson);
 		
 		// add start + end positions to items
-
 		Site.loadingItems(Site.items, Site.container)
 
 	})
@@ -27,37 +25,70 @@ function init(){
 	})
 }
 
-Site.loadingItems = (items, container) => {
-	items.forEach((item, index) => {	
-		container.insertAdjacentHTML('beforeend', Site.itemComponent(item, index))
-	})
+Site.arrayOrder = (array) => {
+	var i = array.length, k , temp;
+	while(--i > 0){
+		k = Math.floor(Math.random() * (i+1));
+		temp = array[k];
+		array[k] = array[i];
+		array[i] = temp;
+	}
+  // shuffled array:
+	return array;
+}	
+
+Site.loadItem = (item, index, container) => {
+	container.insertAdjacentHTML('beforeend', Site.itemComponent(item, index))
 }
 
-Site.generateStyles = () => {
-	var d = new Date();
-	var minute = d.getUTCMinutes()
+Site.loadingItems = (items, container) => {
+	// load in 15 initial items
+	items.forEach((item, index) => {	
+		if(index > 15){ return; }
 
-
+		Site.loadItem(item, index, container)
+	})
 }
 
 Site.itemComponent = (item, index) => {
 	// list item gets one class for animation keyframes
-	// item fades in and out, added to animatino keyframes
-	// list item gets one class defines start and end using translation
-	// ten points of starting on each side
+	// for all animations:
+	// 1-3 position positive translations
+	// 4-6 position negative translations
+	var lean = Math.ceil(Math.random()*3),
+			sideArray = [["top horizontal", "down"], ["bottom horizontal", "up"], ["left vertical", "right"], ["right vertical", "left"]],
+			startPosition = Math.floor(Math.random()*11),
+			leaning = (startPosition > 5) ? lean + 3 : lean,
+			side = Math.floor(Math.random()*4),
+			bobbing = Math.ceil(Math.random()*3);
+
+
 	// 2 possible animations per side and direction, upper lower and middle
-	// middle gets either
 	// item gets inline styles for speed
+	var speed = 50 + Math.round(Math.random()*5),
+			delay = Math.round(Math.random()*15),
+			bobbingSpeed = 15 + Math.round(Math.random()*5),
+			bobbingDelay = Math.round(Math.random()*10);
 	// item gets inline-styles for easing
 
-	// item_image gets inline styles for bobbing speed
-	// item_image also has an inline bobbing delay
-	// item_image gets inline-styles for bobbing easing
-	// Lazy loading of images
+	// bob bezier
+	var bx1 = 0,
+			by1 = Math.random()*0.4,
+			bx2 = Math.random(),
+			by2 = (bx2 < 0.6) ? bx2 + (Math.random() * 0.4) : (Math.random() * 0.4)
 
-	return `<li class="item left vertical1" style="animation: right3 infinite linear 10s;" id="item-${index}">
+
+	return `<li 
+		class="item ${sideArray[side][0] + startPosition}" 
+		style="animation: ${sideArray[side][1] + leaning} cubic-bezier(0.25, 0.46, 0.45, 0.94) ${speed}s ${delay}s;" 
+		id="item-${index}">
 		<a href="${item.url}">
-			<img loading="lazy" class="item_image" src="${item.imagepath}">
+			<img 
+				loading="lazy" 
+				class="item_image" 
+				src="${item.imagepath}"
+				style="animation: bobbing${bobbing} cubic-bezier(${bx1}, ${by1}, ${bx2}, ${by2}) ${bobbingSpeed}s ${bobbingDelay}s;"
+				>
 			<aside class="text_container">
 				<h1 class="item_title item_text">${item.title}</h1>
 				<div class="categories ${item.category.toLowerCase()}">
